7de8627816e89284ef40643765b21f4464cd1f69
Batoul <batoulalso68@gmail.com>
2026-07-15 10:22:57 -0400

    removed unnecessary comments

--- src/MenuItem.java
+++ src/MenuItem.java
@@ -1,16 +1,13 @@
 public class MenuItem {
   
-  // Variable declarations
   public String item_name;
   public double item_price;
 
-  // Override default constructor
   public MenuItem(String name, double price) {
     item_name = name;
     item_price = price;
   }
 
-  // Override default toString
   public String toString() {
     return (item_name + " " + item_price);
   }
--- src/Order.java
+++ src/Order.java
@@ -1,18 +1,15 @@
 public class Order {
 
-  // Variable declarations
   public String customer_name;
   public int drink_selection;
   public int size_selection;
 
-  //Override default constructor
   public Order(String name, int drink, int size) {
     customer_name = name;
     drink_selection = drink;
     size_selection = size;
   }
 
-  //Override default toString
   public String toString() {
     return (customer_name + " " + Cafe.listOfDrinks.get(drink_selection) + Cafe.listOfSizes.get(size_selection));
   }
