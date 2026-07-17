0e3cfe6e3563023dc8e1163b4c82b0760f99e626
Batoul <batoulalso68@gmail.com>
2026-07-17 11:15:25 -0400

    added sql connecter, added class to connect to sql database

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+  private static final String URL = "jdbc:mysqlL//localhost:3306/cafe_db";
+  private static final String USER_NAME = "root";
+  private static final String PASSWORD = "";
+
+  public static Connection getConnection() throws SQLException {
+    return DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+  }
+}
