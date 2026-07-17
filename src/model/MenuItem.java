8d5b4fd2c2a3871f731a4065357de21077b80e48
Batoul <batoulalso68@gmail.com>
2026-07-17 12:45:46 -0400

    added id field, removed setters, made variables private final

--- src/model/MenuItem.java
+++ src/model/MenuItem.java
@@ -1,37 +1,39 @@
 package model;
 public class MenuItem {
   
-  public String itemName;
-  public double itemPrice;
+  private final int itemId;
+  private final String itemName;
+  private final double itemPrice;
 
-  public MenuItem(String name, double price) {
+  public MenuItem(int id, String name, double price) {
     // Input validation
     if (name == null || name.isBlank()) {
       throw new IllegalArgumentException("Item name can't be blank.");
     }    
 
+    if (id < 0) {
+      throw new IllegalArgumentException("Id can't be negative");
+    }
+
     if (price < 0) {
       throw new IllegalArgumentException("Price can't be negative (we can't owe you money...)");
     }
 
+    this.itemId = id;
     this.itemName = name;
     this.itemPrice = price;
   }
 
+  public int getItemId() {
+    return itemId;
+  }
+  
   public String getItemName() {
     return itemName;
   }
 
   public double getItemPrice() {
     return itemPrice;
-  }
-
-  public void setItemName(String name) {
-    itemName = name;
-  }
-
-  public void setItemPrice(double price) {
-    itemPrice = price;
   }
 
   public String toString() {
